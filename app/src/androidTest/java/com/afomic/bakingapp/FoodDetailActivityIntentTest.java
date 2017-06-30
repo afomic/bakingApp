636a53b01f4e75a08cbf8eab5f627740fe786cfc
Afolabi <afomic1@gmail.com>
2017-06-30 03:12:02 +0100

    i made a configuration change

--- app/src/androidTest/java/com/afomic/bakingapp/FoodDetailActivityIntentTest.java
+++ app/src/androidTest/java/com/afomic/bakingapp/FoodDetailActivityIntentTest.java
@@ -10,35 +10,10 @@ import org.junit.Test;
 import static android.support.test.espresso.action.ViewActions.click;
 
 /**
- * Created by afomic on 6/29/17.
+ * Created by afomic on 6/29/17.\
+ *
  */
 
 public class FoodDetailActivityIntentTest {
-    @Rule
-    public IntentsTestRule<FoodDetailActivity> intentsTestRule = new IntentsTestRule<>(
-            FoodDetailActivity.class);
 
-
-    // Registers any resource that needs to be synchronized with Espresso before the test is run.
-    @Before
-    public void registerIdlingResource() {
-
-//        // Stub all external intents
-        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
-
-    }
-
-    /**
-     * Clicks on a RecyclerViewItem and checks if it has intent with extra with the key RECIPE_EXTRA
-     */
-    @Test
-    public void clickRecyclerViewItemHasIntentWithAKey() {
-
-        // Click on the Recipe List RecyclerView item at position 1
-        onView(withId(R.id.)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
-
-        //Checks if the key is present
-        intended(hasExtraWithKey(RECIPE_EXTRA));
-
-    }
 }
--- app/src/androidTest/java/com/afomic/bakingapp/MainActivityIdlingTest.java
+++ app/src/androidTest/java/com/afomic/bakingapp/MainActivityIdlingTest.java
@@ -3,7 +3,34 @@ package com.afomic.bakingapp;
 /**
  * Created by afomic on 6/29/17.
  */
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 public class MainActivityIdlingTest {
 
 }
